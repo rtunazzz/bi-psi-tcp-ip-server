b0df1c4f3c0c7be1e7ff85871dad7cbcffb08845
Artur Hnát <hnatartu@fit.cvut.cz>
2021-03-28 17:38:40 +0200

    implement a barebone conn server

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
 func main() {
-	println("Initialized.")
+	network_type := "tcp"
+	network_addr := ":4000"
+
+	// Create a listener on the 
+	listener, err := net.Listen(network_type, network_addr)
+	if err != nil {
+		log.Fatal("Failed to create a listener:", err)
+		return
+	}
+	
+	// Close when done
+	defer listener.Close()
+	
+	log.Printf("[%s] [%s] Initialized!", strings.ToUpper(network_type), network_addr)
+
+	// Handle incoming connections
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			if err := conn.Close(); err != nil {
+				log.Println("failed to close listener:", err)
+			}
+			continue
+		}
+		log.Println("Connected to", conn.RemoteAddr())
+
+		// go handleConn ...
+	}
 }
