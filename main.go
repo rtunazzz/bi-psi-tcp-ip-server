33e0c5da1c9494c4be6231129e4234342b9b6a32
Artur Hnát <hnatartu@fit.cvut.cz>
2021-03-28 19:12:42 +0200

    add a basic handleConnection func

--- main.go
+++ main.go
@@ -12,29 +12,41 @@ func main() {
 	network_addr := ":4000"
 
 	// Create a listener on the 
-	listener, err := net.Listen(network_type, network_addr)
+	ln, err := net.Listen(network_type, network_addr)
 	if err != nil {
 		log.Fatal("Failed to create a listener:", err)
 		return
 	}
 	
 	// Close when done
-	defer listener.Close()
+	defer ln.Close()
 	
 	log.Printf("[%s] [%s] Initialized!", strings.ToUpper(network_type), network_addr)
 
 	// Handle incoming connections
 	for {
-		conn, err := listener.Accept()
+		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println(err)
-			if err := conn.Close(); err != nil {
-				log.Println("failed to close listener:", err)
+			
+			// Close connection
+			err := conn.Close();
+			if err != nil {
+				log.Println("Failed to close listener:", err)
 			}
 			continue
 		}
 		log.Println("Connected to", conn.RemoteAddr())
 
-		// go handleConn ...
+		go handleConnection(conn)
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer func () {
+		err := conn.Close();
+		if err != nil {
+			log.Println("Failed to close listener:", err)
+		}
+	}()
 }
