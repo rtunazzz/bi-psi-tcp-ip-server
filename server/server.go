f54438c7b7eab1e6a70e1030a34a304aad3db838
Artur Hnát <hnatartu@fit.cvut.cz>
2021-04-04 11:03:56 +0200

    update reader to string, implement message parsing

--- server/server.go
+++ server/server.go
@@ -3,8 +3,6 @@ package server
 // TODO: Find out if we really need to read bytes or not
 
 import (
-	"bufio"
-	"bytes"
 	"log"
 	"net"
 	"strings"
@@ -12,7 +10,8 @@ import (
 )
 
 const (
-	TIMEOUT = 1 * time.Second
+	BUFFER_SIZE        = 1024
+	TIMEOUT            = 1 * time.Second
 	TIMEOUT_RECHARGING = 5 * time.Second
 )
 
@@ -21,16 +20,16 @@ func StartListener() {
 	network_type := "tcp"
 	network_addr := ":4000"
 
-	// Create a listener on the 
+	// Create a listener on the
 	ln, err := net.Listen(network_type, network_addr)
 	if err != nil {
 		log.Fatal("Failed to create a listener:", err)
 		return
 	}
-	
+
 	// Close when done
 	defer ln.Close()
-	
+
 	log.Printf("[%s] [%s] Initialized!", strings.ToUpper(network_type), network_addr)
 
 	// Handle incoming connections
@@ -38,9 +37,9 @@ func StartListener() {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Println("Failed to accept an incoming connection:", err)
-			
+
 			// Close connection
-			err := conn.Close();
+			err := conn.Close()
 			if err != nil {
 				log.Println("Failed to close listener:", err)
 			}
@@ -52,55 +51,61 @@ func StartListener() {
 	}
 }
 
-
-// dropCR drops a terminal \r from the data.
-func dropCR(data []byte) []byte {
-	if len(data) > 0 && data[len(data)-1] == '\r' {
-		return data[0 : len(data)-1]
-	}
-	return data
-}
-
-func ScanCRLF(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-	if i := bytes.Index(data, []byte{'\a', '\b'}); i >= 0 {
-		// We have a full newline-terminated line.
-		return i + 2, dropCR(data[0:i]), nil
-	}
-	// If we're at EOF, we have a final, non-terminated line. Return it.
-	if atEOF {
-		return len(data), dropCR(data), nil
-	}
-	// Request more data.
-	return 0, nil, nil
-}
-
 func handleConnection(conn net.Conn) {
 	log.Printf("[%s] Handling a new connection...", conn.RemoteAddr().String())
 
-	defer func () {
+	defer func() {
 		log.Println("Closing connection...")
-		err := conn.Close();
+		err := conn.Close()
 		if err != nil {
 			log.Println("Failed to close listener:", err)
 		}
 	}()
 
-	scanner := bufio.NewScanner(conn)
-	scanner.Split(ScanCRLF)
-	for scanner.Scan() {
+	strBuffer := ""
+	for {
 		// Set a deadline for reading. Read operation will fail if no data is received after deadline.
 		// conn.SetReadDeadline(time.Now().Add(TIMEOUT))
 
-		temp := string(scanner.Text());
-		log.Printf("temp is: %s\n", temp)
-		if temp == "STOP" {
-			break
+		recBuffer := make([]byte, BUFFER_SIZE)
+		n, err := conn.Read(recBuffer)
+		if n == 0 || err != nil {
+			log.Println("Failed to read connection:", err)
+			return
+		}
+		if e, ok := err.(interface{ Timeout() bool }); ok && e.Timeout() {
+			log.Println("Timeout error", e)
+			return
+		}
+
+		// For debugging
+		// strBuffer += strings.Replace(string(recBuffer[:n]), "\n", "", -1)
+
+		// Convert the received buffer to string and add it to the main buffer
+		strBuffer += string(recBuffer[:n])
+
+		log.Printf("strBuffer:'%s'\n", strBuffer)
+
+		msg, rest := parseMessage(strBuffer)
+		if msg != "" {
+			log.Printf("Message is:'%s'\n", msg)
+			log.Printf("Left to read is:'%s'\n", rest)
+			handleMessage(msg)
+			strBuffer = rest
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		log.Println("Error reading input:", err)
+}
+
+func parseMessage(s string) (msg, rest string) {
+	parts := strings.SplitN(s, "\\a\\b", 2)
+	if len(parts) != 2 {
+		return "", s
 	}
+	msg = parts[0]
+	rest = parts[1]
+	return
+}
+
+func handleMessage(msg string) {
+
 }
